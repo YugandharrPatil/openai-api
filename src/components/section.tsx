ae0b579874d9bb5360809cbab08a5065161cf438
yugz <yugandhar.patil@learner.manipal.edu>
2024-03-04 19:46:52 +0530

    first commit

---
+++ src/components/section.tsx
@@ -0,0 +1,31 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+type SectionPropTypes = {
+	title: string;
+	buttons: {
+		id: number;
+		text: string;
+		link: string;
+		enabled: boolean;
+	}[];
+};
+
+export default function Section(props: SectionPropTypes) {
+	// console.log(props);
+	return (
+		<Card className="p-4 my-8">
+			<h1 className="text-2xl text-center font-semibold mb-3">{props.title}</h1>
+			<div className="flex gap-4 w-fit mx-auto">
+				{props.buttons.map((button: any) => (
+					<Button disabled={!button.enabled} key={button.id}>
+						<Link target="_blank" href={button.link}>
+							{button.text}
+						</Link>
+					</Button>
+				))}
+			</div>
+		</Card>
+	);
+}
---
+++ src/data/buttons.ts
@@ -0,0 +1,199 @@
+export const CHAT_BUTTONS = {
+	title: "Chat",
+	buttons: [
+		{
+			id: 1,
+			text: "Chat with GPT-3.5",
+			link: "#",
+			enabled: true,
+		},
+	],
+};
+
+export const AUDIO_BUTTONS = {
+	title: "Audio",
+	buttons: [
+		{
+			id: 1,
+			text: "Text-to-Audio",
+			link: "#",
+			enabled: false,
+		},
+		{
+			id: 2,
+			text: "Audio-to-Text",
+			link: "#",
+			enabled: false,
+		},
+		{
+			id: 3,
+			text: "Audio to Translated Text",
+			link: "#",
+			enabled: false,
+		},
+	],
+};
+
+export const EMBEDDINGS_BUTTONS = {
+	title: "Embeddings",
+	buttons: [
+		{
+			id: 1,
+			text: "Create Embeddings from Text",
+			link: "#",
+			enabled: false,
+		},
+	],
+};
+
+export const FINE_TUNING_BUTTONS = {
+	title: "Fine Tuning",
+	buttons: [
+		{
+			id: 1,
+			text: "Create a Fine Tuning Job",
+			link: "#",
+			enabled: false,
+		},
+		{
+			id: 2,
+			text: "List all Current Jobs",
+			link: "#",
+			enabled: false,
+		},
+		{
+			id: 3,
+			text: "Get Status of a Job",
+			link: "#",
+			enabled: false,
+		},
+		{
+			id: 4,
+			text: "Get Info of a Job",
+			link: "#",
+			enabled: false,
+		},
+		{
+			id: 5,
+			text: "Cencel a Job",
+			link: "#",
+			enabled: false,
+		},
+	],
+};
+export const FILES_BUTTONS = {
+	title: "Files",
+	buttons: [
+		{
+			id: 1,
+			text: "Upload a File",
+			link: "#",
+			enabled: false,
+		},
+		{
+			id: 2,
+			text: "List A User's Files",
+			link: "#",
+			enabled: false,
+		},
+		{
+			id: 3,
+			text: "Retreive a File's Info",
+			link: "#",
+			enabled: false,
+		},
+		{
+			id: 4,
+			text: "Delete a File",
+			link: "#",
+			enabled: false,
+		},
+		{
+			id: 5,
+			text: "Retreive a File's Content",
+			link: "#",
+			enabled: false,
+		},
+	],
+};
+export const IMAGES_BUTTONS = {
+	title: "Images",
+	buttons: [
+		{
+			id: 1,
+			text: "Generate Image from Text Prompt",
+			link: "#",
+			enabled: false,
+		},
+		{
+			id: 2,
+			text: "Create Image Edit",
+			link: "#",
+			enabled: false,
+		},
+		{
+			id: 3,
+			text: "Generate Image Variations",
+			link: "#",
+			enabled: false,
+		},
+		{
+			id: 4,
+			text: "Get Info of a Job",
+			link: "#",
+			enabled: false,
+		},
+		{
+			id: 5,
+			text: "Cencel a Job",
+			link: "#",
+			enabled: false,
+		},
+	],
+};
+export const MODELS_BUTTONS = {
+	title: "Models",
+	buttons: [
+		{
+			id: 1,
+			text: "List all Models",
+			link: "#",
+			enabled: false,
+		},
+		{
+			id: 2,
+			text: "Retreive a Model's Info",
+			link: "#",
+			enabled: false,
+		},
+		{
+			id: 3,
+			text: "Delete a Model",
+			link: "#",
+			enabled: false,
+		},
+		{
+			id: 4,
+			text: "Get Info of a Job",
+			link: "#",
+			enabled: false,
+		},
+		{
+			id: 5,
+			text: "Cencel a Job",
+			link: "#",
+			enabled: false,
+		},
+	],
+};
+export const MODERATIONS_BUTTONS = {
+	title: "Moderations",
+	buttons: [
+		{
+			id: 1,
+			text: "Create a Moderation",
+			link: "#",
+			enabled: false,
+		},
+	],
+};
